58c4b60bdcbfb7c9d63d2a8928eee2470e4dc75f
이진규 <dlwlsrb0307@naver.com>
2023-08-04 16:16:33 +0900

    회원가입 회원로그인

---
+++ src/main/java/com/green/babyfood/sign/model/SignEntity.java
@@ -0,0 +1,15 @@
+package com.green.babyfood.sign.model;
+
+import lombok.Data;
+
+@Data
+public class SignEntity {
+    private String email;
+    private String password;
+    private String name;
+    private String mobileNb;
+//    private String role;
+    private String address;
+    private String addressDetail;
+    private String nickNm;
+}
---
+++ src/main/java/com/green/babyfood/sign/model/SigninDto.java
@@ -0,0 +1,9 @@
+package com.green.babyfood.sign.model;
+
+import lombok.Data;
+
+@Data
+public class SigninDto {
+    private String email;
+    private String password;
+}
