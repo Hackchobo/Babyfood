801feecb4cc8753c019970bb26b4bb13e23dab6f
홍기윤 <wlwl753@naver.com>
2023-08-10 09:46:26 +0900

    이미지 http:// 추가

--- src/test/java/com/green/babyfood/buy/BuyServiceTest.java
+++ src/test/java/com/green/babyfood/buy/BuyServiceTest.java
@@ -22,52 +22,52 @@ import static org.mockito.BDDMockito.given;
 @Import({BuyService.class})
 class BuyServiceTest {
 
-    @MockBean
-    private BuyMapper mapper;
+//  @MockBean
+//  private BuyMapper mapper;
 
-    @Autowired
-    private BuyService service;
+//  @Autowired
+//  private BuyService service;
 
-    @Test
-    @DisplayName("BuyService- 상품구매")
-    void buyProduct() {
-        //given
-        BuyEntity entity = new BuyEntity();
+//  @Test
+//  @DisplayName("BuyService- 상품구매")
+//  void buyProduct() {
+//      //given
+//      BuyEntity entity = new BuyEntity();
 
-        List<BuyOrderbasketDto> orderbasket = new ArrayList<>();
+//      List<BuyOrderbasketDto> orderbasket = new ArrayList<>();
 
-        BuyOrderbasketDto dto = new BuyOrderbasketDto();
-        BuyOrderbasketDto dto2 = new BuyOrderbasketDto();
+//      BuyOrderbasketDto dto = new BuyOrderbasketDto();
+//      BuyOrderbasketDto dto2 = new BuyOrderbasketDto();
 
-        entity.setReceiver("수령인");
-        entity.setAddress("주소1");
-        entity.setAddressDetail("주소2");
-        entity.setPhoneNm("010-1111-1111");
-        entity.setRequest("요청사항");
-        entity.setIuser(1L);
-        entity.setPoint(1000);
+//      entity.setReceiver("수령인");
+//      entity.setAddress("주소1");
+//      entity.setAddressDetail("주소2");
+//      entity.setPhoneNm("010-1111-1111");
+//      entity.setRequest("요청사항");
+//      entity.setIuser(1L);
+//      entity.setPoint(1000);
 
-        dto.setCartId(1L);
-        dto.setProductId(1L);
-        dto.setIuser(1L);
-        dto.setCount(3);
-        dto.setTotalprice(4500);
+//      dto.setCartId(1L);
+//      dto.setProductId(1L);
+//      dto.setIuser(1L);
+//      dto.setCount(3);
+//      dto.setTotalprice(4500);
 
-        dto2.setCartId(2L);
-        dto2.setProductId(2L);
-        dto2.setIuser(1L);
-        dto2.setCount(1);
-        dto2.setTotalprice(1000);
+//      dto2.setCartId(2L);
+//      dto2.setProductId(2L);
+//      dto2.setIuser(1L);
+//      dto2.setCount(1);
+//      dto2.setTotalprice(1000);
 
-        orderbasket.add(dto);
-        orderbasket.add(dto2);
+//      orderbasket.add(dto);
+//      orderbasket.add(dto2);
 
-        entity.setOrderbasket(orderbasket);
-        service.BuyProduct(entity);
-
-
+//      entity.setOrderbasket(orderbasket);
+//      service.BuyProduct(entity);
 
 
 
-    }
+
+
+//    }
 }
