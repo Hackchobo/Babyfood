91154b37dffb6d506203c2f31bac3ae06c282d65
김다율 <kdy12061004@gmail.com>
2023-08-10 12:58:33 +0900

    유저정보 수정기능 업데이트/원하는 값만 변경(null, 공백예외적용)-김다율

--- src/main/java/com/green/babyfood/mypage/MypageService.java
+++ src/main/java/com/green/babyfood/mypage/MypageService.java
@@ -106,11 +106,17 @@ public class MypageService {
     }
 
     public int UpdProfileDto(ProfileUpdDto dto){
-
-        if (dto.getPassword()!=null || dto.getPassword()!=""){
-            String encode = PW_ENCODER.encode(dto.getPassword());
-            dto.setPassword(encode);
-        }
+        // 빈값으로 들어온 데이터 null 로 변경
+//
+//        if (dto.getNickNm() == null || dto.getNickNm().isEmpty()) {
+//            dto.setNickNm(null);
+//        }
+//        if (dto.getPassword() == null || dto.getPassword().isEmpty()) {
+//            dto.setPassword(null);
+//        }
+//        if (dto.getPhoneNumber() == null || dto.getPhoneNumber().isEmpty()) {
+//            dto.setPhoneNumber(null);
+//        }
 
         return mapper.Updprofile(dto);
     }
