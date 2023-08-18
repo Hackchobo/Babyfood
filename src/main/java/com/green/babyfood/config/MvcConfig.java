bfe6f977478dc90127da2241fe0c6199e69648d4
이진규 <dlwlsrb0307@naver.com>
2023-08-18 10:26:58 +0900

    프론트 빌드

--- src/main/java/com/green/babyfood/config/MvcConfig.java
+++ src/main/java/com/green/babyfood/config/MvcConfig.java
@@ -29,3 +29,38 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/img/**").addResourceLocations("file:"+ uploadPath + "/");
     }
 }
+
+
+/*package com.green.smartGrade.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import java.io.IOException;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Value("${file.dir}")
+    private String fileDir;
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry reg) {
+        reg.addResourceHandler("/**", "/imgs/**")
+                .addResourceLocations("classpath:/static/**", "file:" + fileDir+"/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource requestedResource = location.createRelative(resourcePath);
+                        // If we actually hit a file, serve that. This is stuff like .js and .css files.
+                        if (requestedResource.exists() && requestedResource.isReadable()) {
+                            return requestedResource;
+                        }
+                        // Anything else returns the index.
+                        return new ClassPathResource("/static/index.html");
+                    }
+                });
+        //reg.addResourceHandler("/imgs/**").addResourceLocations("file:" + fileDir+"/")
+    }
+}*/
